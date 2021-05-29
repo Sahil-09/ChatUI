4c20adb7b3da49f077ef3cd1e44c9edcd1d7f78b
Sahil <patelsahil9768@gmail.com>
2021-05-29 22:00:59 +0530

    Responsive update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WoobChat';
+  isopen = false;
+  issideopen = true;
+  open(){
+    this.isopen=!this.isopen
+  }
+
+  sideopen(event){
+    console.log("emitted app")
+    this.issideopen=!this.issideopen
+  }
 }
--- src/app/profileview/profileview.component.ts
+++ src/app/profileview/profileview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-profileview',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileviewComponent implements OnInit {
 
+  @Output() closeemit:EventEmitter<any>= new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  close(){
+    console.log("emitted pr")
+    this.closeemit.emit(null)
   }
 
 }
